7b0076f5065a8f6306e003a43466eeb824786ce7
Stan <dimitrov.s.dev@gmail.com>
2023-04-28 22:03:41 +0300

    Telerik_Alpha_Python

--- Mock_Exams/1/Prime_Triangle.py
+++ Mock_Exams/1/Prime_Triangle.py
@@ -1,1 +1,26 @@
+n = int(input())
 
+values_list = []
+
+for number in range(1, n + 1):
+    values_list.append(number)
+
+prime_num = []
+
+for number in values_list:
+    condition = True
+    for i in range(2, number):
+        if number % i == 0:
+            condition = False
+    if condition:
+        prime_num.append(number)
+
+for row in range(len(prime_num)):
+    for col in range(1, prime_num[row] + 1):
+        if col in prime_num:
+            col = 1
+        else:
+            col = 0
+        print(col, end="")
+
+    print()
